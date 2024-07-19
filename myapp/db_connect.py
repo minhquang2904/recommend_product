6ef91727e7b4cb5db05ffe3955e2d1187f4ad561
LuongMinhQuang <minhquangnthq@gmail.com>
2024-07-19 10:33:26 +0700

    no message

--- myapp/db_connect.py
+++ myapp/db_connect.py
@@ -1,18 +1,10 @@
-import os
+# myapp/db_connect.py
 import pymongo
 from pymongo import MongoClient
-from pathlib import Path
-from dotenv import load_dotenv
-
-env_path = Path('.') / '.env.local'
-
-load_dotenv(dotenv_path=env_path)
 
 
 def get_database():
-    mongo_url = os.getenv('MONGO_URL')
-    print(mongo_url)
-    client = MongoClient('mongodb+srv://minhquang29042001:daVCWUZcsTOOCyEY@store.goz72zm.mongodb.net/store')
+    client = MongoClient("mongodb+srv://minhquang29042001:daVCWUZcsTOOCyEY@store.goz72zm.mongodb.net/store")
     return client['store']
 
 def get_products_collection():
